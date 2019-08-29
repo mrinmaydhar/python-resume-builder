a5a3126d0ca8b2c9a71eae7b4a5878a7c07ef7e5
Dave Welch <david.m.welch@gmail.com>
2019-08-29 18:05:38 -0500

    Update to Python 3  - Added command line helper  - Added regression test  - Tested with Python 3.7

--- build_resume.py
+++ build_resume.py
@@ -1,60 +1,67 @@
 #!/usr/bin/env python
+"""
+Python Resume Builder
 
-#
-# see readme for more instructions.
-#
+Usage:
+  build_resume.py <source> <outdir>
+  build_resume.py -h | --help
+  build_resume.py --version
 
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+
+"""
 import sys
-import imp
+import importlib.util
 import os.path
 
+from docopt import docopt
 import jinja2
 
+
 def add_ascii_data(resume):
-    """takes a resume datastructure and add some data to the
+    """ 
+    Takes a resume datastructure and adds some data to the
     hashes for ascii formatting.
 
-This function exists because the source file does not store 
-whitespace information, but it's required for the ascii output.
-It's easiest to put it in the data structure and let the
-templates read and render it as necessary."""
-
+    This function exists because the source file does not store 
+    whitespace information, but it's required for the ascii output.
+    It's easiest to put it in the data structure and let the
+    templates read and render it as necessary.
+    """
     for hash in resume['TECHNICAL_SKILLS']:
         hash['ascii_whitespace'] = " " * (25 - len(hash['category']))
-
     for hash1 in resume['WORK_EXPERIENCE']:
         for hash2 in hash1['positions']:
             hash2['ascii_whitespace'] = " " * (26 - len(hash2['name']))
 
-def main():
-    """render templates using sourcefile.
-
-takes the arg passed at the command line and saves output
-there.  uses export type to determine the template name
-and what filename to give the output."""
-
+def main(sourceFile, outdir):
+    """
+    Render the templates using the source file.
+    Takes the arg passed at the commandline and saves output
+    there.  Uses export type to determine the template name
+    and what filename to give the output.
+    """
     # parse argument filepath
-    SOURCE_FILE = sys.argv[1]
-    name, ext = os.path.splitext(SOURCE_FILE)
-    SAVE_AS = os.path.basename(name)
-
+    name, ext = os.path.splitext(sourceFile)
+    SAVE_AS = os.path.join(os.path.relpath(outdir), os.path.basename(name))
     # import the argument as a python module
-    resume_module = imp.load_source('resume', SOURCE_FILE)
-
+    spec = importlib.util.spec_from_file_location('resume', sourceFile)
+    resume_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(resume_module)
     add_ascii_data(resume_module.RESUME)
-
     # render templates
     template_types = ['html','tex','txt']
-
     for ttype in template_types:
         # open the template file, make it an object
         template_string = open('templates/' + ttype + '_template.' + ttype, 'r').read()
         template = jinja2.Template(template_string)
-
         # write the rendered template to file
-        output = open(SAVE_AS + '.' + ttype, 'w')
-        output.write(template.render(resume_module.RESUME, mode=ttype))
-        output.close()
+        filename = SAVE_AS + '.' + ttype
+        with open(filename, 'w') as output:
+            output.write(template.render(resume_module.RESUME, mode=ttype))
 
 if __name__ == '__main__':
-    main()
+    args = docopt(__doc__, help=True, version='1.0', options_first=False)
+    main(args["<source>"], args["<outdir>"])
